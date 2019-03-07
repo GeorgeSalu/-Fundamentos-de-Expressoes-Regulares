d9e1f226fd123e9aa2c280c9746f19e7570cd938
George Salu <george.salu10@gmail.com>
2019-03-07 19:19:11 -0300

    quantificador *

---
+++ quantificadores/ZeroMais.js
@@ -0,0 +1,6 @@
+const text1 = 'De longe eu avistei o fogo e uma pessoa gritando: FOGOOOOOOO'
+const text2 = 'There is a big fog in NYC'
+
+const regex = /fogo*/gi
+console.log(text1.match(regex))
+console.log(text2.match(regex))
